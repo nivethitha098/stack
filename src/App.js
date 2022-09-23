2b56832f9fb7b6165733f5e05b5e2be894d6b56f
Nivethitha <nivethitha098@gmail.com>
2022-09-23 20:04:26 +0530

    Revert "Navbar completed"
    This reverts commit 74cceac8bc15bb28d49e66293d2a83a2ec2c9584.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import AllRoutes from './AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <AllRoutes />
-      </BrowserRouter>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
